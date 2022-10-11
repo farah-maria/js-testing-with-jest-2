8d8aef63a9c924107c11b5f0cecda1cec54ce1e4
Farah M <farah.maria.rahman@gmail.com>
2022-10-11 23:58:53 +0000

    complete mock

--- swapiGetter.test.js
+++ swapiGetter.test.js
@@ -2,10 +2,15 @@ import swapiGetter from "./swapiGetter.js";
 import mockAxios from "axios";
 import { async } from "regenerator-runtime";
 
+jest.mock("axios");
+mockAxios.get.mockResolvedValue({ data: { name: "Mock Jedi"}})        
+
 describe("swapiGetter", () => {
+    afterEach(jest.clearAllMocks); 
+
     test("should return a name", async () => {
-        const result = await swapiGetter(1)
-        console.log(result)
-        expect(result).toBe("Luke Skywalker");
+        const result = await swapiGetter(1) //await async or promise-chaining
+        expect(result).toBe("Mock Jedi");
+        expect(mockAxios.get).toHaveBeenCalledTimes(1);
     });
 });
