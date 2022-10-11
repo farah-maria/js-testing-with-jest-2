074fd6060a19d900a1d29256d00a5f6f4dffd5aa
Farah M <farah.maria.rahman@gmail.com>
2022-10-11 13:56:41 +0000

    add swapiGetter id

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+import swapiGetter from "./swapiGetter.js";
+import mockAxios from "axios";
 import axios from "axios";
 
-// get a star wars person by id and return their name
 
+// get a star wars person by id and return their name
+const swapiGetter = (id) =>
+    axios.get(`http:swapi.dev/api/people/${id}/`).then((res) => res.data.name);
+
+export default swapiGetter;
