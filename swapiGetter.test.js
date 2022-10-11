0b43918e314b3ec3e3520e31218f755c8722f8b6
Farah M <farah.maria.rahman@gmail.com>
2022-10-11 20:02:54 +0000

    add async and await to test to get it to pass

--- swapiGetter.test.js
+++ swapiGetter.test.js
@@ -1,7 +1,11 @@
 import swapiGetter from "./swapiGetter.js";
 import mockAxios from "axios";
+import { async } from "regenerator-runtime";
 
 describe("swapiGetter", () => {
-    test("should return a name", () => {
+    test("should return a name", async () => {
+        const result = await swapiGetter(1)
+        console.log(result)
+        expect(result).toBe("Luke Skywalker");
     });
 });
